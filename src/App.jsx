0038f05c089a00eee2f1f6538b3a878f2652c011
Kylee <Kykybrown2002@gmail.com>
2023-11-11 01:31:33 -0600

    files set up and ready to develope

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Home from './components/Home';
+import Services from './components/About';
+
+function App() {
+  return (
+    <>
+    
+
+    <BrowserRouter>
+    <h1>Hello World</h1>
+    <Link to='/'>Home</Link>
+    <br></br>
+    <Link to='/services'>Services</Link>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/services" element={<Services/>}></Route>
+      </Routes>
+    </BrowserRouter>
+    </>
+  )
+
+}
+
+export default App
---
+++ src/components/About.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Services = () => {
+    return <h1> Services Page Here</h1>
+}
+
+export default Services
